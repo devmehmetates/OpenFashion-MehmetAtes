cca3fbd51bafdfa5e97794454f14444a2e0fe17b
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-07-29 18:35:49 +0300

    Initial commit and configure .gitignore

---
+++ OpenFashion-MehmetAtes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenFashion-MehmetAtes
+//
+//  Created by Mehmet Ateş on 29.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
