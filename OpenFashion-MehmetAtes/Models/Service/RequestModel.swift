9cf9ff0a29475bf23639c137959c099475fd275f
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-07-30 22:06:42 +0300

    add network service and request model

---
+++ OpenFashion-MehmetAtes/Models/Service/RequestModel.swift
@@ -0,0 +1,10 @@
+//
+//  RequestModel.swift
+//  OpenFashion-MehmetAtes
+//
+//  Created by Mehmet Ateş on 30.07.2022.
+//
+
+protocol RequestModel: Codable { }
+
+struct DummyModel: RequestModel { }
---
+++ OpenFashion-MehmetAtes/Utils/Services/NetworkService.swift
@@ -0,0 +1,38 @@
+//
+//  NetworkService.swift
+//  OpenFashion-MehmetAtes
+//
+//  Created by Mehmet Ateş on 30.07.2022.
+//
+
+import Alamofire
+
+class NetworkService<T: RequestModel> {
+    
+    static func apiRequest(
+        _ path: String, method: HTTPMethod? = .get,
+        parameters: T? = nil,
+        headers: HTTPHeaders? = nil,
+        interceptor: RequestInterceptor? = nil,
+        requestModifier: Session.RequestModifier? = nil,
+        result: @escaping(Result<Data, AFError>) -> Void
+    ) {
+        AF.request(
+            path,
+            method: method ?? .get,
+            parameters: parameters,
+            encoder: JSONParameterEncoder.default,
+            headers: headers,
+            interceptor: interceptor,
+            requestModifier: requestModifier
+        ).response { data in
+            switch data.result {
+            case .success(let data):
+                guard let data = data else { return }
+                return result(.success(data))
+            case .failure(let err):
+                return result(.failure(err))
+            }
+        }
+    }
+}
