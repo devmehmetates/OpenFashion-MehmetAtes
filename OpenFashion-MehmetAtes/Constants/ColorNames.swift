3b33010d731be6ea060dac653eb4f239513e1711
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-07-30 23:18:00 +0300

    add color names constants

---
+++ OpenFashion-MehmetAtes/Constants/ColorNames.swift
@@ -0,0 +1,17 @@
+//
+//  ColorNames.swift
+//  OpenFashion-MehmetAtes
+//
+//  Created by Mehmet Ateş on 30.07.2022.
+//
+
+struct ColorNames {
+    static let accentColor = "AccentColor"
+    static let backgroundColor = "Background"
+    static let bodyColor = "BodyColor"
+    static let inputBackgroundColor = "InputBackground"
+    static let labelColor = "Label"
+    static let lineColor = "LineColor"
+    static let placeholderColor = "Placeholder"
+    static let secondarColor = "Secondary"
+}
