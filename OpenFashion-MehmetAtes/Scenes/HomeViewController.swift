90ec8fb34b3f1d277f0e7933605a7938a7204231
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-07-31 17:37:36 +0300

    add explore collection button

--- OpenFashion-MehmetAtes/Scenes/HomeViewController.swift
+++ OpenFashion-MehmetAtes/Scenes/HomeViewController.swift
@@ -31,6 +31,7 @@ extension HomeViewController {
     private func configureContent() {
         configureScrollView()
         configureImageSliderView()
+        configureExploreCollectionButton()
         configureArrivalDivider()
     }
     
@@ -49,6 +50,32 @@ extension HomeViewController {
         contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
         contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
         contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+    }
+    
+    private func configureExploreCollectionButton() {
+        let exploreCollectionButton = UIButton()
+        exploreCollectionButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        exploreCollectionButton.setTitle("Explore Collection", for: .normal)
+        exploreCollectionButton.titleLabel?.font = UIFont(name: AppConstants.fontName, size: 20)
+        exploreCollectionButton.backgroundColor = UIColor(named: ColorNames.labelColor)?.withAlphaComponent(0.85)
+        exploreCollectionButton.layer.cornerRadius = 15
+        contentView.addSubview(exploreCollectionButton)
+        
+        exploreCollectionButton.bottomAnchor.constraint(equalTo: imageSliderView.bottomAnchor, constant: -40).isActive = true
+        exploreCollectionButton.centerXAnchor.constraint(equalTo: imageSliderView.centerXAnchor).isActive = true
+        exploreCollectionButton.leadingAnchor.constraint(equalTo: imageSliderView.leadingAnchor, constant: 60).isActive = true
+        exploreCollectionButton.bottomAnchor.constraint(equalTo: imageSliderView.bottomAnchor, constant: 60).isActive = true
+        
+        exploreCollectionButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+    }
+    
+    @objc
+    func buttonAction() {
+        let viewac = UIAlertController(title: "Okay", message: "Deneme", preferredStyle: .alert)
+        viewac.addAction(UIAlertAction(title: "OK", style: .destructive))
+        
+        present(viewac, animated: true)
     }
     
     private func configureImageSliderView() {
@@ -78,7 +105,7 @@ extension HomeViewController {
         
         arrivalTitle.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         arrivalTitle.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
-        arrivalTitle.topAnchor.constraint(equalTo: imageSliderView.bottomAnchor, constant: 50).isActive = true
+        arrivalTitle.topAnchor.constraint(equalTo: imageSliderView.bottomAnchor, constant: 30).isActive = true
     
         arrivalDivider.drawDividerShape()
         contentView.addSubview(arrivalDivider)
