d760395b0c58795eaf04f13a36ae00a340b93841
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-07-31 12:42:36 +0300

    setup tab bar controller and configure their buttons

---
+++ OpenFashion-MehmetAtes/Scenes/SomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SomeViewController.swift
+//  OpenFashion-MehmetAtes
+//
+//  Created by Mehmet Ateş on 31.07.2022.
+//
+
+import UIKit
+
+class SomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
