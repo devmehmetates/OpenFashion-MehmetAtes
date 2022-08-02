2e6b9d5bbbadf80603cc72ac9ed186fd8c4ac54b
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-08-02 17:52:56 +0300

    add some constants

---
+++ OpenFashion-MehmetAtes/Constants/NetworkConstants.swift
@@ -0,0 +1,10 @@
+//
+//  NetworkConstants.swift
+//  OpenFashion-MehmetAtes
+//
+//  Created by Mehmet Ateş on 2.08.2022.
+//
+
+import Foundation
+
+func productsPath(_ offset: Int, limit: Int) -> String { return "https://api.escuelajs.co/api/v1/products/?offset=\(offset)&limit=\(limit)" }
---
+++ OpenFashion-MehmetAtes/Utils/Extensions/ViewBounds.swift
@@ -0,0 +1,13 @@
+//
+//  ViewBounds.swift
+//  OpenFashion-MehmetAtes
+//
+//  Created by Mehmet Ateş on 2.08.2022.
+//
+
+import UIKit
+
+extension Double {
+    var responsiveH: Double { return (UIScreen.main.bounds.size.height * self) / 100 }
+    var responsiveW: Double { return (UIScreen.main.bounds.size.width * self) / 100 }
+}
